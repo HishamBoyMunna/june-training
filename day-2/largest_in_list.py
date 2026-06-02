4bf174c21223040aef9e0e7839c77579d9ffbf0e
HishamBoyMunna <158536380+HishamBoyMunna@users.noreply.github.com>
2026-06-02 09:58:12 +0530

    finished check largest in list

---
+++ day-2/largest_in_list.py
@@ -0,0 +1,14 @@
+lst = [7,5,8,3,4,26,3,22,1]
+
+larg = lst[0]
+larg2 = lst[0]
+
+for i in range(len(lst)):
+    
+    if lst[i] > larg:
+        #print(lst[i])
+        larg2 = larg
+        larg = lst[i]
+    elif lst[i] > larg2:
+        larg2 = lst[i]
+print(f"Largest: {larg}\nSecond: {larg2}")
